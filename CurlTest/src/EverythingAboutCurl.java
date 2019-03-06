82776d7505aed5e47f9488717594840e5f872767
Minh <minhta16@augustana.edu>
2019-03-06 17:23:04 -0600

    Done Java curl for time track

---
+++ CurlTest/src/EverythingAboutCurl.java
@@ -0,0 +1,70 @@
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.OutputStreamWriter;
+
+public class EverythingAboutCurl {
+	public static void main(String[] args) {
+		String userToken = "TUlOSFRBMTZAYXVndXN0YW5hLmVkdQ==:eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxNzUzMzI2LCJnZW5lcmF0ZWRfYXQiOiIyMDE5LTAzLTA2IDE5OjEzOjE5In0.DxvUav8KRxixHbAAMZw5n6Kq19mzOJCc58h2cd1uViFqELmhZ2aj7shKuqR-K6Z58K6BsCLdmP4-XpETCtksfg";
+//		newIncident(userToken);
+		addTimeTrack(userToken, 35961711, 30);
+	}
+
+	public static void newIncident(String userToken) {
+		try {
+			String url = "https://api.samanage.com/incidents.xml";
+
+			URL obj = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+			conn.setDoOutput(true);
+
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("X-Samanage-Authorization", "Bearer " + userToken);
+			conn.setRequestProperty("Accept", "application/vnd.samanage.v2.1+xml");
+			conn.setRequestProperty("Content-Type", "text/xml");
+
+			String data = "<incident>" + " <name>Test</name>" + " <priority>Medium</priority>"
+					+ " <requester><email>minhta16@augustana.edu</email></requester>"
+					+ " <category><name>Meetings (ITS use only)</name></category>" + " <subcategory>"
+					+ "      <name>Training/Workshops</name>" + " </subcategory>"
+					+ " <description>Test curl-ing new incidents/description>"
+					+ " <assignee><email>minhta16@augustana.edu</email></assignee>" + "</incident>";
+			OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+			out.write(data);
+			out.close();
+
+			new InputStreamReader(conn.getInputStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void addTimeTrack(String userToken, int incidentID, int time) {
+
+		try {
+			String url = "https://api.samanage.com/incidents/" + incidentID + "/time_tracks.xml";
+
+			URL obj = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+			conn.setDoOutput(true);
+
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("X-Samanage-Authorization", "Bearer " + userToken);
+			conn.setRequestProperty("Accept", "application/vnd.samanage.v2.1+xml");
+			conn.setRequestProperty("Content-Type", "text/xml");
+
+			String data = 	"<time_track>" + 
+								"<name>slow</name>" + 
+								"<creator_id>1753326</creator_id>" + 
+								"<minutes_parsed>" + time + "</minutes_parsed>" + 
+							"</time_track>";
+			OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+			out.write(data);
+			out.close();
+
+			new InputStreamReader(conn.getInputStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- ShellScript/curl.sh
+++ ShellScript/curl.sh
@@ -6,12 +6,35 @@ TOKEN=TUlOSFRBMTZAYXVndXN0YW5hLmVkdQ==:eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxNzUz
 
 #curl -H "X-Samanage-Authorization: Bearer $TOKEN" -H 'Accept: application/vnd.samanage.v2.1+xml' -H 'Content-Type:text/xml' -X GET https://api.samanage.com/incidents/16602/time_tracks.xml
 
-curl -H "X-Samanage-Authorization: Bearer TUlOSFRBMTZAYXVndXN0YW5hLmVkdQ==:eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxNzUzMzI2LCJnZW5lcmF0ZWRfYXQiOiIyMDE5LTAzLTA2IDE5OjEzOjE5In0.DxvUav8KRxixHbAAMZw5n6Kq19mzOJCc58h2cd1uViFqELmhZ2aj7shKuqR-K6Z58K6BsCLdmP4-XpETCtksfg" -d '
-<time_track>
-  <name>slow</name>
-  <creator_id>1753326</creator_id>
-  <minutes_parsed>25</minutes_parsed>
-</time_track>
-' -H 'Accept: application/vnd.samanage.v2.1+xml' -H 'Content-Type:text/xml' -X POST https://api.samanage.com/incidents/35881048/time_tracks.xml
+# curl -H "X-Samanage-Authorization: Bearer $TOKEN" -d '
+# <incident>
+#  <name>Test</name>
+#  <priority>Medium</priority>
+#  <requester><email>minhta16@augustana.edu</email></requester>
+#  <category><name>Meetings (ITS use only)</name></category>
+#  <subcategory>
+#       <name>Training/Workshops</name>
+#  </subcategory>
+#  <description>Test curl-ing new incidents/description>
+#  <assignee><email>minhta16@augustana.edu</email></assignee>
+# </incident>
+# ' -H 'Accept: application/vnd.samanage.v2.1+xml' -H 'Content-Type:text/xml' -X POST https://api.samanage.com/incidents.xml
 
-curl -H "X-Samanage-Authorization: Bearer TUlOSFRBMTZAYXVndXN0YW5hLmVkdQ==:eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoxNzUzMzI2LCJnZW5lcmF0ZWRfYXQiOiIyMDE5LTAzLTA2IDE5OjEzOjE5In0.DxvUav8KRxixHbAAMZw5n6Kq19mzOJCc58h2cd1uViFqELmhZ2aj7shKuqR-K6Z58K6BsCLdmP4-XpETCtksfg" -H 'Accept: application/vnd.samanage.v2.1+xml' -H 'Content-Type:text/xml' -X GET https://api.samanage.com/incidents/35881048/time_tracks.xml -o testtimetrack.xml
+curl -H "X-Samanage-Authorization: Bearer $TOKEN" -d '
+<incident>
+ <name>Test</name>
+ <priority>Medium</priority>
+ <requester><email>minhta16@augustana.edu</email></requester>
+ <description>Test curl-ing new incidents/description>
+</incident>
+' -H 'Accept: application/vnd.samanage.v2.1+xml' -H 'Content-Type:text/xml' -X POST https://api.samanage.com/incidents.xml
+
+# curl -H "X-Samanage-Authorization: Bearer $TOKEN" -d '
+# <time_track>
+#   <name>slow</name>
+#   <creator_id>1753326</creator_id>
+#   <minutes_parsed>25</minutes_parsed>
+# </time_track>
+# ' -H 'Accept: application/vnd.samanage.v2.1+xml' -H 'Content-Type:text/xml' -X POST https://api.samanage.com/incidents/35881048/time_tracks.xml
+
+# curl -H "X-Samanage-Authorization: Bearer $TOKEN" -H 'Accept: application/vnd.samanage.v2.1+xml' -H 'Content-Type:text/xml' -X GET https://api.samanage.com/incidents/35881048/time_tracks.xml -o testtimetrack.xml
