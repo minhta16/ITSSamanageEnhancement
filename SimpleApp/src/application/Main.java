c03fa10f519d595bf02e3c48e67672b544721bd4
Minh <minhta16@augustana.edu>
2019-03-08 16:15:11 -0600

    added simple app

---
+++ SimpleApp/src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+	
+import java.io.IOException;
+
+import application.controller.MainPaneController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/view/MainPane.fxml"));
+        Parent root = (Parent)loader.load();
+        Scene scene = new Scene(root);
+    
+        
+        MainPaneController controller = (MainPaneController)loader.getController();
+        controller.setStageAndSetupListeners(primaryStage);
+        
+        primaryStage.setTitle("Simple App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
